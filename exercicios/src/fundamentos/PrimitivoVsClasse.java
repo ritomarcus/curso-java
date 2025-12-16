dd813a082ea032ce3276a82a52fc546c33f38cbd
marcus rito <marcus.rito@Paulispell.local>
2025-12-16 11:16:16 -0300

    Primivos e Classe

---
+++ exercicios/src/fundamentos/PrimitivoVsClasse.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class PrimitivoVsClasse {
+
+	public static void main(String[] args) {
+		
+		String s = "texto";
+		s.toUpperCase();
+		
+		// Wrappers são a versão objeto dos tipos
+		// primitivos!
+		int a = 123;
+		System.out.println(a);	
+	}
+}
