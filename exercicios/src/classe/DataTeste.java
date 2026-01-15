90cd06059c5123c03a4fefc716e7616e5b86f980
Marcus Rito <rito.marcus@hotmail.com>
2026-01-15 13:31:09 -0300

    Classes

---
+++ exercicios/src/classe/DataTeste.java
@@ -0,0 +1,21 @@
+package classe;
+
+public class DataTeste {
+
+	public static void main(String[] args) {
+
+		Data d1 = new Data();
+
+		d1.dia = 2;
+		d1.mes = 11;
+		d1.ano = 2025;
+
+		var d2 = new Data();
+		d2.dia = 11;
+		d2.mes = 07;
+		d2.ano = 2001;
+
+		System.out.printf("%d/%d/%d\n", d1.dia, d1.mes, d1.ano);
+		System.out.printf("%d/%d/%d", d2.dia, d2.mes, d2.ano);
+	}
+}
