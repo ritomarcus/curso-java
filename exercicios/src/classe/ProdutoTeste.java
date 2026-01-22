f479bbad092ff17891e632dacd7bdeae8cbfd7e4
Marcus Rito <rito.marcus@hotmail.com>
2026-01-22 11:33:30 -0300

    Membros de Classe e Instância

--- exercicios/src/classe/ProdutoTeste.java
+++ exercicios/src/classe/ProdutoTeste.java
@@ -4,15 +4,16 @@ public class ProdutoTeste {
 
 	public static void main(String[] args) {
 
-		Produto p1 = new Produto("Notebook", 4383.90, 0.25);
+		Produto p1 = new Produto("Notebook", 4356.89);
 
 		var p2 = new Produto();
 		p2.nome = "Chinelo";
-		p2.preco = 21.54;
-		p2.desconto = 0.29;
+		p2.preco = 12.56;
+		
+		Produto.desconto = 0.50;
 
-		System.out.println(p1.nome);
-		System.out.println(p2.nome);
+		System.out.println(p1.nome + " " + p1.precoComDesconto());
+		System.out.println(p2.nome + " " + p2.precoComDesconto());
 
 		double precoFinal1 = p1.precoComDesconto();
 		double precoFinal2 = p2.precoComDesconto(0.1);
