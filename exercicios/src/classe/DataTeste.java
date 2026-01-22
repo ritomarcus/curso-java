a716df6b7f706a5de7e71c1342a6cfdadd6c6141
Marcus Rito <rito.marcus@hotmail.com>
2026-01-22 09:59:17 -0300

    Construtores em Java e Desafio de construtores

--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -5,21 +5,15 @@ public class DataTeste {
 	public static void main(String[] args) {
 
 		Data d1 = new Data();
-
-		d1.dia = 2;
-		d1.mes = 11;
-		d1.ano = 2025;
-
-		var d2 = new Data();
-		d2.dia = 11;
-		d2.mes = 07;
-		d2.ano = 2001;
+		d1.ano = 2021;
+		
+		var d2 = new Data(31, 12, 2020);
 
 		String dataFormatada1 = d1.obterDataFormatada();
 
 		System.out.println(dataFormatada1);
 		System.out.println(d2.obterDataFormatada());
-		
+
 		d1.imprimirDataFormatada();
 		d2.imprimirDataFormatada();
 	}
